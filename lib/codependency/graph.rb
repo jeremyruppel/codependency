56cf2bbcfd65e12d8927369c35dbfde7d6bc5a49
Jeremy Ruppel <jeremy.ruppel@gmail.com>
2012-05-09 08:57:12 -0700

    Get the graph implementation working

--- lib/codependency/graph.rb
+++ lib/codependency/graph.rb
@@ -1,5 +1,28 @@
 module Codependency
   class Graph
+    def initialize( filenames )
+      @nodes = Hash[ filenames.map { |f| [ f, Node.new( f ) ] } ]
+    end
+    attr_reader :nodes
 
+    def files
+      nodes.reduce( [ ] ) do |list, (filename, node)|
+        resolve node, list
+        list
+      end
+    end
+
+    protected
+
+    def resolve( node, list )
+      unless list.include? node.filename
+        node.dependencies.each do |dep|
+          # TODO need to check here to make sure
+          # we actually have a node for this dependency
+          resolve nodes[ dep ], list
+        end
+        list << node.filename
+      end
+    end
   end
 end
--- spec/codependency/graph_spec.rb
+++ spec/codependency/graph_spec.rb
@@ -4,14 +4,13 @@ describe Codependency::Graph do
   let( :path ){ File.join( File.dirname( __FILE__ ), '../fixtures' ) }
   subject { Codependency::Graph.new Dir[ File.join( path, '*.rb' ) ] }
 
-  pending
-  # its( :files ){ should eq(
-  #   [
-  #     "#{path}/body.rb",
-  #     "#{path}/planet.rb",
-  #     "#{path}/earth.rb",
-  #     "#{path}/mars.rb",
-  #     "#{path}/phobos.rb"
-  #   ]
-  # ) }
+  its( :files ){ should eq(
+    [
+      "#{path}/body.rb",
+      "#{path}/planet.rb",
+      "#{path}/earth.rb",
+      "#{path}/mars.rb",
+      "#{path}/phobos.rb"
+    ]
+  ) }
 end
