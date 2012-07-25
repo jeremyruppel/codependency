4d3853386131acb49a39ef6de86ce8ef2a3cf906
Jeremy Ruppel <jeremy.ruppel@gmail.com>
2012-07-25 16:32:08 -0700

    Remove filesystem dependency for parser spec

--- spec/codependency/parser_spec.rb
+++ spec/codependency/parser_spec.rb
@@ -1,31 +1,71 @@
 require 'spec_helper'
 
 describe Codependency::Parser do
-  let( :path   ){ File.join( File.dirname( __FILE__ ), '../fixtures' ) }
   let( :parser ){ Codependency::Parser.new }
 
+  before do
+    IO.stub( :readlines ) do |arg|
+      case File.basename( arg, '.rb' ).to_sym
+      when :body
+        """
+        class Body
+        end
+        """
+      when :earth
+        """
+        # require planet
+
+        class Earth
+        end
+        """
+      when :mars
+        """
+        # require planet
+
+        class Mars
+        end
+        """
+      when :phobos
+        """
+        # require body
+        # require mars
+
+        class Phobos
+        end
+        """
+      when :planet
+        """
+        # require body
+
+        class Planet
+        end
+        """
+      end.strip.split( /^\s+/ )
+    end
+  end
+
   context 'body' do
-    subject { parser.parse( "#{path}/body.rb" ) }
+    subject { parser.parse( "body.rb" ) }
     it { should eq( [ ] ) }
   end
 
   context 'earth' do
-    subject { parser.parse( "#{path}/earth.rb" ) }
+    subject { parser.parse( "earth.rb" ) }
     it { should eq( [ 'planet' ] ) }
   end
 
   context 'mars' do
-    subject { parser.parse( "#{path}/mars.rb" ) }
+    subject { parser.parse( "mars.rb" ) }
     it { should eq( [ 'planet' ] ) }
   end
 
   context 'phobos' do
-    subject { parser.parse( "#{path}/phobos.rb" ) }
+    subject { parser.parse( "phobos.rb" ) }
     it { should eq( [ 'body', 'mars' ] ) }
   end
 
   context 'planet' do
-    subject { parser.parse( "#{path}/planet.rb" ) }
+    subject { parser.parse( "planet.rb" ) }
     it { should eq( [ 'body' ] ) }
   end
 end
