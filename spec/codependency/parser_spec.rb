4b58247af0bf2d6cd343c8d1285c6765df63116d
Jeremy Ruppel <jeremy.ruppel@gmail.com>
2012-07-25 17:15:17 -0700

    Remove fixtures from specs

--- spec/codependency/parser_spec.rb
+++ spec/codependency/parser_spec.rb
@@ -3,27 +3,27 @@ require 'spec_helper'
 describe Codependency::Parser, :files => :planets do
   let( :parser ){ Codependency::Parser.new }
 
-  context 'body' do
+  context 'body dependencies' do
     subject { parser.parse( "body.rb" ) }
     it { should eq( [ ] ) }
   end
 
-  context 'earth' do
+  context 'earth dependencies' do
     subject { parser.parse( "earth.rb" ) }
     it { should eq( [ 'planet' ] ) }
   end
 
-  context 'mars' do
+  context 'mars dependencies' do
     subject { parser.parse( "mars.rb" ) }
     it { should eq( [ 'planet' ] ) }
   end
 
-  context 'phobos' do
+  context 'phobos dependencies' do
     subject { parser.parse( "phobos.rb" ) }
     it { should eq( [ 'body', 'mars' ] ) }
   end
 
-  context 'planet' do
+  context 'planet dependencies' do
     subject { parser.parse( "planet.rb" ) }
     it { should eq( [ 'body' ] ) }
   end
