57d1667f30d376b3ee82f1d48e722d8f87280bfb
Jeremy Ruppel <jeremy.ruppel@gmail.com>
2012-09-24 15:27:41 -0700

    Remove unnecessary Node class

--- lib/codependency.rb
+++ lib/codependency.rb
@@ -2,6 +2,5 @@ require 'codependency/version'
 
 module Codependency
   autoload :Graph,  'codependency/graph'
-  autoload :Node,   'codependency/node'
   autoload :Parser, 'codependency/parser'
 end
